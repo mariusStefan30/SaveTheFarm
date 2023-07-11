905162a61d0389c85ca0608aca633ed507c76430
mariusStefan30 <stefanmariusadrian@yahoo.com>
2023-07-11 19:41:29 +0100

    first commit

---
+++ pickMushrooms.php
@@ -0,0 +1,18 @@
+<?php
+  
+function pickMushrooms(){
+	// Write your code here:
+  global $location, $has_mushrooms;
+
+  if ( $location !== "woods"){
+    echo "These aren't any mushrooms to pick!\n";
+  } else {
+    echo "You pick some mushrooms.\n";
+    $has_mushrooms = TRUE;
+  }
+  
+  
+  
+  
+  
+}  
